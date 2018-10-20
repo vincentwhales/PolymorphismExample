9c7cdc7006c9a16115e1eb5734bbb0a622a57472
Vincent Whales <vincent@whales.media>
2018-10-19 21:25:14 -0700

    Added model definition

--- polyex/settings.py
+++ polyex/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projects',
+    'polymorphic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
